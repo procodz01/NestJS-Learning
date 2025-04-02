c07ec2158ee9ff69c5adadea2a3aba806e14b60b
procodz01 <raghvendra.kumar@mobilecoderz.com>
2025-04-02 15:25:11 +0530

    nest

---
+++ src/users/user.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+
+@Module({}) //decorator
+// This is a decorator that marks the class as a NestJS module
+// It takes an object as an argument, which can contain properties like imports, controllers, providers, etc.
+export class UserModule{
+
+}
